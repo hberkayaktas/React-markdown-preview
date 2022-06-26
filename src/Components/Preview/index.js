0d492fc50622216f5e4eadd816dbf7efd92edc3b
Hami Berkay Aktaş <hberkayaktas@gmail.com>
2022-06-27 00:45:37 +0300

    react mark down a geçildi

--- src/Components/Preview/index.js
+++ src/Components/Preview/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import {selectText } from '../../redux/textSlice';
+import {selectText,itemSafhali } from '../../redux/textSlice';
 import { useSelector } from 'react-redux';
-
+import ReactMarkdown from 'react-markdown'
+import ReactDom from 'react-dom'
 
 function Preview() {
       const StateText = useSelector(selectText);
+      const itemSafhaliIn = useSelector(itemSafhali);
   return (
       <div className="col-md-5 holder">
-      <div className="text-panel-right" id="preview" dangerouslySetInnerHTML={{ __html: StateText.toString().replace(/,/g,"") }}>
+      <div className="text-panel-right" id="preview" /* dangerouslySetInnerHTML={{ __html: StateText.toString().replace(/,/g,"") }} */>
             {/* {selectText} */}
             {/* {StateText} */}
+            <ReactMarkdown>{itemSafhaliIn}</ReactMarkdown>
       </div>
     </div>
   );
